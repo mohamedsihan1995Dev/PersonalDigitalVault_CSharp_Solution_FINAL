0c64594eb20089037a4c0a78f587c21108ebbd48
Thusha <jesusthusha1@gmail.com>
2026-08-10 20:21:21 +0530

    Revert "updating"
    This reverts commit fe1e00fde91027bfd47a10295a90f88bbd76b929.

--- backend/PersonalDigitalVault.Api/DTOs/Admin/UpdateUserStatusDto.cs
+++
@@ -1,7 +0,0 @@
-namespace PersonalDigitalVault.Api.DTOs.Admin;
-public class UpdateUserStatusDto 
-{
-    // true  = Enable
-    // false = Disable
-    public bool IsActive { get; set; }
-}
--- backend/PersonalDigitalVault.Api/wwwroot/js/pages/dashboard.js
+++
@@ -1,1 +0,0 @@
-import{requireAuth}from'../auth/authGuard.js';import{initNavbar}from'../components/navbar.js';requireAuth();initNavbar();
